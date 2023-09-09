c28a82c14c144ed37e31adbe9a90e1d19b851b87
Kasim Abubakar Jajere <alkasima@gmail.com>
2023-09-09 20:36:55 +0100

    Change json order and output status code as integer

--- src/main/java/com/alkasima/getrequest/springgetrequest/api/controller/RequestController.java
+++ src/main/java/com/alkasima/getrequest/springgetrequest/api/controller/RequestController.java
@@ -15,6 +15,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Locale;
 import java.util.Map;
 
@@ -26,30 +27,38 @@ public class RequestController {
     @ResponseBody
    public Object getObject(@RequestParam("slack_name") String slackName, @RequestParam("track") String track) {
 
-        // Get the current time in UTC+2
-        LocalDateTime utcTime = LocalDateTime.now().plusHours(2);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
-        String formattedUtcTime = utcTime.format(formatter);
+         // Get the current time in UTC+2
+         LocalDateTime utcTime = LocalDateTime.now().plusHours(2);
+         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+         String formattedUtcTime = utcTime.format(formatter);
 
-        //Variable for github file and repo
-        String git_repo_url = "https://github.com/alkasima/SpringGetRequest";
-        String git_file_ulr = "https://github.com/alkasima/SpringGetRequest/blob/main/src/main/java/com/alkasima/getrequest/springgetrequest/api/controller/RequestController.java";
+         //Variable for github file and repo
+         String git_repo_url = "https://github.com/alkasima/SpringGetRequest";
+         String git_file_ulr = "https://github.com/alkasima/SpringGetRequest/blob/main/src/main/java/com/alkasima/getrequest/springgetrequest/api/controller/RequestController.java";
 
-        //Get current day
-        LocalDate currentDate = LocalDate.now();
-        DayOfWeek dayOfWeek = currentDate.getDayOfWeek();
-        String dayName = dayOfWeek.getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+         //Get current day
+         LocalDate currentDate = LocalDate.now();
+         DayOfWeek dayOfWeek = currentDate.getDayOfWeek();
+         String dayName = dayOfWeek.getDisplayName(TextStyle.FULL, Locale.ENGLISH);
 
 
-        //Build object for output
-        Map<String, Object> object = new HashMap<>();
-        object.put("slack_name", slackName);
-        object.put("current_day", dayName);
-        object.put("utc_time", formattedUtcTime);
-        object.put("track", track);
-        object.put("github_file_url", git_file_ulr);
-        object.put("github_repo_url", git_repo_url);
-        object.put("status_code", 200);
-        return object;
+         //Build object for output
+         Map<String, Object> object = new LinkedHashMap<>();
+         object.put("slack_name", slackName);
+         object.put("current_day", dayName);
+         object.put("utc_time", formattedUtcTime);
+         object.put("track", track);
+         object.put("github_file_url", git_file_ulr);
+         object.put("github_repo_url", git_repo_url);
+         object.put("status_code", 200); // Use an integer for the status_code
+
+         // Convert the LinkedHashMap to a JSON string
+         String jsonString = "{" + object.entrySet().stream()
+                 .map(entry -> "\"" + entry.getKey() + "\": " + entry.getValue())
+                 .reduce((entry1, entry2) -> entry1 + ", " + entry2)
+                 .orElse("") + "}";
+
+          return jsonString;
+
     }
 }
